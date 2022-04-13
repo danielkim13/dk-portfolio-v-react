f121da548b35a9faa047c45f0fac67bec42c001b
Daniel Kim <daniel.d.kim13@gmail.com>
2022-04-12 23:18:55 -0400

    figured out how to use bulma css framework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
-import './App.css';
+import "bulma/css/bulma.min.css";
+import "./App.css";
 
 function App() {
-  return <div>
-    <h1>REACT Project</h1>
-  </div>
+  return (
+    <div>
+      <h1 className="is-size-1">REACT Project</h1>
+      <h2>with Bulma CSS Framework</h2>
+    </div>
+  );
 }
 
 export default App;
