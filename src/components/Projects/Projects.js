26d45cb40e82863106aa5bf932f9c7a60e7e32f2
Daniel Kim <daniel.d.kim13@gmail.com>
2022-04-17 13:58:46 -0400

    final touch

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,15 +2,11 @@ import Project from "./Project";
 import { projects } from "./constants.js";
 
 export default function Projects() {
-  
-
   return (
-    <section>
-      <div className="columns m-3 is-mobile is-flex-wrap-wrap">
-        {projects?.map((project) => (
-          <Project key={project.name} name={project.name} profileImage={project.profileImage} image={project.image} description={project.description} gitHub={project.gitHub} deployed={project.deployed} contributor={project.contributor} />
-        ))}
-      </div>
+    <section className="columns m-3 is-mobile is-flex-wrap-wrap">
+      {projects?.map((project) => (
+        <Project key={project.name} name={project.name} profileImage={project.profileImage} image={project.image} description={project.description} gitHub={project.gitHub} deployed={project.deployed} contributor={project.contributor} />
+      ))}
     </section>
   );
 }
