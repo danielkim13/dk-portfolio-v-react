4df2e4eadee3b17e7dbc379f31e5378d5cf88393
Daniel Kim <daniel.d.kim13@gmail.com>
2022-04-17 20:19:20 -0400

    was testing something out and forgot to reverted it back to orig code

--- src/App.js
+++ src/App.js
@@ -15,46 +15,26 @@ function App() {
     document.title = capitalizeFirstLetter(tab);
   }, [tab]);
 
-  let test;
-
-  switch (tab) {
-    case "about":
-      test = <About />;
-      break;
-    case "portfolio":
-      test = <Projects />;
-      break;
-    case "contact":
-      test = <ContactForm />;
-      break;
-    case "resume":
-      test = <Resume />;
-      break;
-    default:
-      test = null;
-  }
-
-  // const RenderPage = () => {
-  //   switch (tab) {
-  //     case "about":
-  //       return <About />;
-  //       break;
-  //     case "portfolio":
-  //       return <Portfolio />;
-  //       break;
-  //     case "contact":
-  //       return <ContactForm />;
-  //     case "resume":
-  //       return <Resume />;
-  //     default:
-  //       return null;
-  //   }
-  // };
+  const RenderPage = () => {
+    switch (tab) {
+      case "about":
+        return <About />;
+        break;
+      case "portfolio":
+        return <Portfolio />;
+        break;
+      case "contact":
+        return <ContactForm />;
+      case "resume":
+        return <Resume />;
+      default:
+        return null;
+    }
+  };
   return (
     <>
       <Header tab={tab} setTab={setTab} />
-      {/* <main>{RenderPage()}</main> */}
-      <main>{test}</main>
+      <main>{RenderPage()}</main>
       <Footer />
     </>
   );
